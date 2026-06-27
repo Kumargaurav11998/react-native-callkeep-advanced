9ac4305b13f2cbf3044638b93aa6f17c8bb42013
Gaurav <gauravojha402@gmail.com>
2026-06-27 22:30:18 +0530

    chore: fix metro config exclusionList error and update example to depend on published 0.0.4

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const root = path.resolve(__dirname, '..');
+const pak = require('../package.json');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const escape = (string) => string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+const modules = Object.keys({ ...pak.peerDependencies });
 
 /**
  * Metro configuration
@@ -6,6 +13,27 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [root],
+  resolver: {
+    unstable_enablePackageExports: false,
+    blacklistRE: exclusionList(
+      modules.map(
+        (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
+    ),
+    extraNodeModules: modules.reduce((acc, name) => {
+
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {
+      [pak.name]: root
+    }),
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(root, 'node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
